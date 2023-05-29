69cfa7c766176a5823e573306e3c3b46bc9bdb14
belgherbi-lina <127565670+belgherbi-lina@users.noreply.github.com>
2023-05-29 20:40:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const emailInput = document.getElementById('email-input');
+const validEmail = document.getElementById('valid-email');
+const button = document.getElementById('btn');
+const dismiss = document.getElementById('btn-2');
+const displayEmailUser = document.getElementById('email-user');
+const succesSign = document.querySelector('.box');
+const successCard = document.querySelector('.subscrib');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  const emailRegx = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})([\.a-z]{2,8})?$/ig;
+  if (emailRegx.test(emailInput.value)) {
+      succesSign.style.display = 'none' ;
+      successCard.style.display = 'block';
+      displayEmailUser.textContent = emailInput.value;
+  }else{
+      validEmail.textContent = 'Valid email required';
+      document.querySelector(".error-").classList.add("error");
+  }
+});
+
+dismiss.addEventListener('click',()=>{
+  successCard.style.display = 'none' ;
+  succesSign.style.display = 'flex';
+  emailInput.value = '';
+})
